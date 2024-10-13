f4d30fddfe61625e87c8d28322927f7e46e1330c
preciousken <pkehinde27@gmail.com>
2024-10-13 13:28:27 +0100

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,66 @@
+// fn main() {
+//     let mut account: BankAccount = BankAccount{
+//         owner:"Precious".to_string(),
+//         balance:1000.00
+//     };
+
+//     // immutable borrow to check the balance
+//     account.check_balance();
+//     account.withdraw(50.75);
+//     account.check_balance();
+// }
+
+// struct BankAccount {
+//     owner: String,
+//     balance:f64,
+// }
+
+// impl BankAccount {
+//     fn withdraw(&mut self, amount: f64) {
+//         println!(
+//             "withdrawing {} from the account owned by {}",
+//             amount, self.owner
+//         );
+//         self.balance -= amount;
+//     }
+
+//     fn check_balance(&self) {
+//         println!("Account balance of {} is {}", self.owner, self.balance);
+//     }
+// }
+
+// fn main(){
+//     const age:i32 = 40;
+//     if (age >= 18){
+//         println!("you can drive a car", );
+
+//     }else {
+//         println!("You can't drive a car", );
+//     }
+// }
+
 fn main() {
-    let mut account: BankAccount = BankAccount{
-        owner:"Precious".to_string(),
-        balance:1000.00
+    struct Book {
+        title: String,
+        author: String,
+        pages: u32,
+        available: bool,
+    }
+
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u64,
+    }
+
+    let mut   user1: User = User { 
+        active: true, 
+        username: String::from("ademola Kehinde"), 
+        email:String::from("email@mail.com"), 
+        sign_in_count: 1 ,
     };
 
-    // immutable borrow to check the balance
-    account.check_balance();
-    account.withdraw(50.75);
-    account.check_balance();
-}
-
-struct BankAccount {
-    owner: String,
-    balance:f64,
-}
-
-impl BankAccount {
-    fn withdraw(&mut self, amount: f64) {
-        println!(
-            "withdrawing {} from the account owned by {}",
-            amount, self.owner
-        );
-        self.balance -= amount;
-    }
-
-    fn check_balance(&self) {
-        println!("Account balance of {} is {}", self.owner, self.balance);
-    }
+    user1.email = String::from("newmail@mail.com");
+    println!("user email is {}",user1.email );
 }
